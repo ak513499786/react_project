0fae357bc34a173c14342c754570782f55360f59
Md Ahmed Ali Khan <79787782+ak513499786@users.noreply.github.com>
2023-02-09 10:23:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './main.css'
+
+import './main.css';
+import {HashRouter as Router} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
 reportWebVitals();
