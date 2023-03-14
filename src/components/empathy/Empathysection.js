8058b57ccb9370c96ff34d3affc539fe140a08a2
ak513499786 <ak513499786@gmail.com>
2023-03-14 13:47:50 +0530

    blogsort

--- src/components/empathy/Empathysection.js
+++ src/components/empathy/Empathysection.js
@@ -2,55 +2,73 @@ export default function Empathyhero() {
   return (
     <section className="empathy-content-container">
       <div className="empathy-content">
-        <h1 className="empathy-h1">Platter Wing Lot Black</h1>
         <p className="empathy-para">
-          Wing parmesan crust garlic style Philly beef. Personal chicken olives
-          cheese party. Mushrooms wing banana wing large pie olives olives
-          lovers. Aussie meatball pineapple chicken anchovies burnt party
-          marinara. Stuffed tomato white ham hand extra lovers party. <br /> Lot
-          onions NY white Philly Philly anchovies. Lot pizza meatball hand pesto
-          Hawaiian broccoli large pan pie. Platter peppers NY stuffed dolor.
-          Hawaiian peppers Aussie ranch red. Mayo tossed melted parmesan red
-          burnt large Philly garlic Philly. Tomato pepperoni ranch anchovies
-          parmesan wing red meat Bianca. Mouth red broccoli platter bbq style
-          pepperoni pizza.
+          Since 1989, Intuit (maker of Quickbooks, Mint, and TurboTax) has run a
+          “Follow me Home ” program that involves questionnaires, observations,
+          interviews, etc. with the actual users at workplaces where Intuit’s
+          products are being used. The goal of the program is to find out how
+          customers use the product, what they like and don’t, and what
+          challenges they may have.
+        </p>
+        <p className="empathy-para">
+          And what does Intuit do with all these insights? They change, modify,
+          or tweak their products. Not kidding. For instance, the 2006 version
+          of one of their products, Quicken, had 121 customer-recommended
+          improvements. [Source]
+        </p>
+        <p className="empathy-para">
+          Harley-Davidson has carefully adjusted the voices of its motors to
+          please its customers’ ears. In fact, they have sued competitors who
+          have imitated their motor voices.
+        </p>
+        <p className="empathy-para">
+          The ability to get into your customers’ shoes results in user empathy.
+          Intuit and Harley-Davidson are just two of the many companies
+          worldwide that have mastered the art of empathy in User Experience
+          (UX) design.
         </p>
       </div>
       <div className="empathy-content">
-        <h1 className="empathy-h1">Platter Wing Lot Black</h1>
+        <h1 className="empathy-h1">What is Empathy?</h1>
         <p className="empathy-para">
-          Wing parmesan crust garlic style Philly beef. Personal chicken olives
-          cheese party. Mushrooms wing banana wing large pie olives olives
-          lovers. Aussie meatball pineapple chicken anchovies burnt party
-          marinara. Stuffed tomato white ham hand extra lovers party. <br /> Lot
-          onions NY white Philly Philly anchovies. Lot pizza meatball hand pesto
-          Hawaiian broccoli large pan pie. Platter peppers NY stuffed dolor.
-          Hawaiian peppers Aussie ranch red. Mayo tossed melted parmesan red
-          burnt large Philly garlic Philly. Tomato pepperoni ranch anchovies
-          parmesan wing red meat Bianca. Mouth red broccoli platter bbq style
-          pepperoni pizza.
+          Empathy is a foundational principle in the concept of design thinking
+          – a user-based approach to problem-solving that combines hands-on
+          testing of a product or service with the actual users.
+        </p>
+        <p className="empathy-para">
+          Empathy is thus a crucial factor in UX design because it involves the
+          usability and function of products, and services in the environment of
+          the users. Empathy allows product design teams to identify which ideas
+          need more testing before they commit to a development or re-design
+          project.
         </p>
       </div>
       <div className="empathy-content">
-        <h1 className="empathy-h1">Platter Wing Lot Black</h1>
+        <h1 className="empathy-h1">Why is Empathy so important in UX?</h1>
         <p className="empathy-para">
-          Wing parmesan crust garlic style Philly beef. Personal chicken olives
-          cheese party. Mushrooms wing banana wing large pie olives olives
-          lovers. Aussie meatball pineapple chicken anchovies burnt party
-          marinara. Stuffed tomato white ham hand extra lovers party. <br /> Lot
-          onions NY white Philly Philly anchovies. Lot pizza meatball hand pesto
-          Hawaiian broccoli large pan pie. Platter peppers NY stuffed dolor.
-          Hawaiian peppers Aussie ranch red. Mayo tossed melted parmesan red
-          burnt large Philly garlic Philly. Tomato pepperoni ranch anchovies
-          parmesan wing red meat Bianca. Mouth red broccoli platter bbq style
-          pepperoni pizza.
+          The story is told of a local cafe in London that was particularly
+          concerned about the complaints the staff was getting about ‘really bad
+          tea. The first and most obvious conclusion that the cafe owner made
+          was that the chef was only three months old. But he couldn’t afford to
+          fire the new chef. So the owner introduced the concept of MYOT (make
+          your own tea) and put it on the menu. Those who opted for MYOT were
+          given hot water in a teapot, a side of milk, sugar, and three kinds of
+          tea leaves. The customers were also asked to set aside a tiny cup of
+          the tea they made for the staff to taste later.
         </p>
-        <h1 className="empathy-h1">Platter Wing Lot Black</h1>
         <p className="empathy-para">
-          Wing parmesan crust garlic style Philly beef. Personal chicken olives
-          cheese party. Mushrooms wing banana wing large pie olives olives
-          lovers. Aussie meatball pineapple chicken anchovies burnt party
-          marinara. Stuffed tomato white ham hand extra lovers party.
+          After many MYOTs, the owner instructed the chef to brew two types of
+          tea leaves, one stronger and one lighter. Interestingly, those who
+          preferred a stronger tea had an Irish background, while those who
+          preferred their tea lighter were mostly London natives.
+        </p>
+        <p className="empathy-para">
+          In case we haven’t made it obvious already, empathy is crucial in UX.
+          However, it does not appear to come naturally to designers in general,
+          and especially to UX designers. In fact, 2009 research stated that
+          professionals working in the technology field have lower empathy. When
+          there’s a disconnect between a designer and the end user, the product
+          or application may fail.
         </p>
       </div>
     </section>
--- src/components/services/services.js
+++ src/components/services/services.js
@@ -39,104 +39,11 @@ export default function Services() {
     });
     tl.fromTo(fade, { opacity: "0" }, { opacity: "1" });
   }, []);
-
-  // <<<<<<< HEAD
-  // =======
-  // useEffect(() => {
-  //   const tl = gsap.timeline({
-  //     repeat: -1,
-  //     ease: Power0,
-  //   });
-  //   tl.fromTo(wordanimation, 2, { y: 0 }, { y: 0 });
-  //   tl.fromTo(wordanimation, 2, { y: 0 }, { y: -97 });
-  //   tl.fromTo(wordanimation, 2, { y: -97 }, { y: -197 });
-  //   tl.fromTo(wordanimation, 2, { y: -197 }, { y: -292 });
-  // }, []);
-
-  const mouseOver = () => {
-    gsap.to(background, 0.3, {
-      css: { backgroundSize: "120%", backgroundPosition: "50% 0" },
-    });
-    gsap.to(title, 0.3, {
-      css: { color: "white", textShadow: "0px 0px 20px white" },
-    });
-  };
-  const mouseOut = () => {
-    gsap.to(background, 0.5, { css: { backgroundSize: "100%" } });
-    gsap.to(title, 0.3, {
-      css: { color: "grey", textShadow: "0px 0px 0px white" },
-    });
-  };
-  const mouseOver1 = () => {
-    gsap.to(background1, 0.3, {
-      css: { backgroundSize: "120%", backgroundPosition: "50% 0" },
-    });
-    gsap.to(title1, 0.3, {
-      css: { color: "white", textShadow: "0px 0px 20px white" },
-    });
-  };
-  const mouseOut1 = () => {
-    gsap.to(background1, 0.5, { css: { backgroundSize: "100%" } });
-    gsap.to(title1, 0.3, {
-      css: { color: "grey", textShadow: "0px 0px 0px white" },
-    });
-  };
-  const mouseOver2 = () => {
-    gsap.to(background2, 0.3, {
-      css: { backgroundSize: "120%", backgroundPosition: "50% 0" },
-    });
-    gsap.to(title2, 0.3, {
-      css: { color: "white", textShadow: "0px 0px 20px white" },
-    });
-  };
-  const mouseOut2 = () => {
-    gsap.to(background2, 0.5, { css: { backgroundSize: "100%" } });
-    gsap.to(title2, 0.3, {
-      css: { color: "grey", textShadow: "0px 0px 0px white" },
-    });
-  };
-  const mouseOver3 = () => {
-    gsap.to(background3, 0.3, {
-      css: { backgroundSize: "120%", backgroundPosition: "50% 0" },
-    });
-    gsap.to(title3, 0.3, {
-      css: { color: "white", textShadow: "0px 0px 20px white" },
-    });
-  };
-  const mouseOut3 = () => {
-    gsap.to(background3, 0.5, { css: { backgroundSize: "100%" } });
-    gsap.to(title3, 0.3, {
-      css: { color: "grey", textShadow: "0px 0px 0px white" },
-    });
-  };
   return (
-    // <<<<<<< HEAD
     <>
       <Navbar />
       <main className="services" ref={(el) => (fade = el)}>
-        {/* <AnimatedCursor color="255, 255, 255"/> */}
-
         <Servicehero />
-        {/* <Wedo /> */}
-        {/* <section className="services-container">
-=======
-    <main className="services">
-      <Navigation_bar />
-      <section id="hero_services" ref={(el) => (fade = el)}>
-        <div className="services_hero-text">
-          <p className="leading">Leading The Way From</p>
-          <div className="overflowcontrol">
-            <div className="rolling-words" ref={(el) => (wordanimation = el)}>
-              <p className="words">Design To Execution</p>
-              <p className="words">Strategy To Execution</p>
-              <p className="words">Development To Execution</p>
-              <p className="words">Marketing To Success</p>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      {/* <Do /> */}
         <section className="wedo">
           <div className="wedo-container">
             <h1 className="wedo-h1">We Do It All!</h1>
@@ -189,83 +96,9 @@ export default function Services() {
             </div>
           </div>
         </section>
-        {/* <section className="services-container">
->>>>>>> cf43176c7766f4b31cb4810966493754162b7074
-        <h1 className="ser-h1">Explore Our Services</h1>
-        <div className="ser-mini">
-          <Swiper
-            spaceBetween={90}
-            className="ser-imgs"
-            direction={"horizontal"}
-            mousewheel={true}
-            releaseOnEdge={true}
-            slidesPerView={3}
-            modules={[Mousewheel]}
-            breakpoints={{
-              640: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-            }}
-          >
-            <div className="ser-imgs">
-              <SwiperSlide className="cns"
-              ref={(el) => (background = el)}
-                  onMouseOver={mouseOver}
-                  onMouseOut={mouseOut}>
-                <Link
-                  to={"/consultingandstrategy"}
-                  className="slide"
-                >
-                  <p className="ser-txt" ref={(el) => (title = el)}>
-                    Consulting And Strategy
-                  </p>
-                </Link>
-              </SwiperSlide>
-              <SwiperSlide className="ue">
-                <Link
-                  to={"/userexperience"}
-                  className="ue"
-                  ref={(el) => (background1 = el)}
-                  onMouseOver={mouseOver1}
-                  onMouseOut={mouseOut1}
-                >
-                  <p className="ser-txt" ref={(el) => (title1 = el)}>
-                    User Experience
-                  </p>
-                </Link>
-              </SwiperSlide>
-              <SwiperSlide className="pd">
-                <Link
-                  to={"/productdevelopment"}
-                  className="pd"
-                  ref={(el) => (background2 = el)}
-                  onMouseOver={mouseOver2}
-                  onMouseOut={mouseOut2}
-                >
-                  <p className="ser-txt" ref={(el) => (title2 = el)}>
-                    Product Development
-                  </p>
-                </Link>
-              </SwiperSlide>
-              <SwiperSlide className="dm">
-                <Link
-                  to={"/digitalmarketing"}
-                  className="dm"
-                  ref={(el) => (background3 = el)}
-                  onMouseOver={mouseOver3}
-                  onMouseOut={mouseOut3}
-                >
-                  <p className="ser-txt" ref={(el) => (title3 = el)}>
-                    Digital Marketing
-                  </p>
-                </Link>
-              </SwiperSlide>
-            </div>
-          </Swiper>
+        <div className="slide-container">
+          <Explore />
         </div>
-      </section> */}
-        <Explore />
         <Model />
       </main>
       <Element id="example" name="example">
