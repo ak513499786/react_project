70a968f1c2829322673da28d7257f932dfbbcddf
ak513499786 <ak513499786@gmail.com>
2023-03-16 10:40:55 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import About from "./components/about/about";
 import Services from "./components/services/services";
-import Home from "./components/Home/Home";
+import Home from "./components/home/Home";
 import Work from "./components/work/Work";
-import Career from "./components/Career/career";
-import Blog from "./components/Blog/Blog";
+import Career from "./components/career/career";
+import Blog from "./components/blog/Blog";
 import Talk from "./components/letsTalk/Talk";
 import Case from "./components/caseStudy/case_study";
 import Empathy from "./components/empathy/empathy";
@@ -16,7 +16,7 @@ import PD from "./components/productDevelopment/productdevelopment";
 import AnimatedCursor from "react-animated-cursor";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Client from "./components/Home/client";
+// import Client from "./components/Home/client";
 
 function App() {
   return (
