5b790817439b8b0cd2d220c953df91b03078a527
ak513499786 <ak513499786@gmail.com>
2023-04-19 10:56:03 +0530

    asa

--- src/components/Bloginnerpages/React/TrendSection.js
+++ src/components/Bloginnerpages/React/TrendSection.js
@@ -112,6 +112,29 @@ export default function Trendsection() {
           marketing in 2023. However, this should be the year of collecting as
           many use cases for your business as possible.
         </p>
+        <p className="empathy-para">
+          Pros of Flutter
+          <div className="points">
+            <p className="empathy-para">
+              - Offers a rich library of widgets 
+            </p>
+            <p className="empathy-para">
+              - Well-structured documentation
+            </p>
+            <p className="empathy-para">
+              - Apps are fast and easy to navigate, and they require less testing
+            </p>
+            <p className="empathy-para">
+              - Apps are fast and easy to navigate, and they require less testing
+            </p>
+            <p className="empathy-para">
+              - Apps are fast and easy to navigate, and they require less testing
+            </p>
+            <p className="empathy-para">
+              - Apps are fast and easy to navigate, and they require less testing
+            </p>
+          </div>
+        </p>
       </div>
       <div className="empathy-content">
         <h1 className="empathy-h1">Information as bites</h1>
@@ -130,89 +153,8 @@ export default function Trendsection() {
           web/app stand out. If you are looking for empathetic UX/UI design, do
           call us.
         </p>
-        <p className="empathy-para">
-          Some things to consider -
-          <div className="points">
-            <p className="empathy-para">
-              - repurpose existing content into bite-sized information
-            </p>
-            <p className="empathy-para">
-              - simplify difficult concepts of your business/product or service
-            </p>
-            <p className="empathy-para">
-              - leverage social media channels to relay this information
-            </p>
-          </div>
-        </p>
       </div>
       <div className="empathy-content">
-        <h1 className="empathy-h1">Influencers aren’t going anywhere</h1>
-        <p className="empathy-para">
-          This trend of businesses leveraging expert individuals to create brand
-          advocacy has been going on for nearly five years.2023 is not going to
-          be an exception. In fact, influencer marketing will continue to trend
-          widely, with some of the world’s biggest brands, and movie makers
-          using influencers to sell products, and entertainment.
-        </p>
-        <p className="empathy-para">
-          Here’s what you can do -
-          <div className="points">
-            <p className="empathy-para">
-              - research opportunities to reach out to your audience first
-            </p>
-            <p className="empathy-para">
-              - short-list influencers who share the same values
-            </p>
-            <p className="empathy-para">
-              - start with a barter relationship with influencers instead of
-              spending money upfront
-            </p>
-          </div>
-        </p>
-      </div>
-      <div className="empathy-content">
-        <h1 className="empathy-h1">Linked-in is a thought influencer in B2B</h1>
-        <p className="empathy-para">
-          There’s no better advocacy for B2B companies than to reach out and
-          influence relevant audiences on Linked-in. And 2023 will be the same.
-          There’s a certain credibility attached to the thought leadership
-          opportunities that Linked-in offers that B2B audiences are looking
-          for. In fact, a study has stated that Linked-in may capture nearly 25%
-          of US B2B advertising spending by 2024. Even as the platform evolves,
-          businesses should leverage the time to build relevant followers who
-          will eventually become their brand advocates.
-        </p>
-        <p className="empathy-para">
-          Some things to do in 2023 -
-          <div className="points">
-            <p className="empathy-para">
-              - use Linkedin’s new features for thought leadership
-            </p>
-            <p className="empathy-para">
-              - create credible Linked-in profiles for your founders.
-            </p>
-            <p className="empathy-para">
-              - post useful content about your industry
-            </p>
-            <p className="empathy-para">
-              - build relationships with competitors
-            </p>
-          </div>
-        </p>
-      </div>
-      <div className="empathy-content">
-        <h1 className="empathy-h1">Not every trend is for your business </h1>
-        <p className="empathy-para">
-          Just like we mentioned about metaverse, not every digital marketing
-          trend is for you. While it is crucial to know beforehand what’s going
-          on in the market, it is all the more significant to contextualize
-          trends for your business. Let your customers be your guiding light
-          about which trends to follow, and you will eventually find the right
-          mix.
-        </p>
-      </div>
-      <div className="empathy-content">
-        <h1 className="empathy-h1">Keep focusing on your foundation </h1>
         <p className="empathy-para">
           A trend that would never go out of fashion is to focus on your
           foundations, such as -
